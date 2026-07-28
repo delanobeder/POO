1629890ffe539f563056d25ea51e9893b40b8e0f
Delano Medeiros Beder <delanobeder@yahoo.com.br>
2026-07-28 10:43:14 -0300

    Refatoração (Versão 2026)

--- Modulo07/Triangulo-Java/Equilatero.java
+++
@@ -1,9 +0,0 @@
-public class Equilatero extends Triangulo {
-    public Equilatero(double lado) {
-        super(lado);
-    }
-
-    public double getArea() {
-        return lados[0] * lados[0] * Math.sqrt(3) / 4;
-    }
-}
